d4ef71b479cec4a6128a9624a2556f916d227662
hatuan1985 <haanhtuan8x@gmail.com>
2026-09-24 00:22:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/theo-doi-minh-chung-mai-son/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
