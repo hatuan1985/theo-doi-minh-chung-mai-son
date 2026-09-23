b317147b739c2ef97ed40bd3fd7916711a4e8e1f
hatuan1985 <haanhtuan8x@gmail.com>
2026-09-24 00:43:02 +0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 // CẤU HÌNH CHẠY THỰC TẾ
 // Google Apps Script Web App URL đã được cấu hình.
 window.MAI_SON_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyau0DuyrxH01l-x2MEpSYsdfJaZafW7TPkbkzxcXA6yAGmoMnFHxcFupJZzAdby0Lp0A/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbw4es7waNpVYgDcxYpX0YDlGVBhMgq_y5lRdLv8Q93NaeWSIrw3UrJIBFncZw2x_yaFWQ/exec",
   DRIVE_FOLDER_URL: "https://drive.google.com/drive/folders/1ZhplPmXeI4ujatTN4S91842kixupZUuu"
 };
